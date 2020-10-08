91ea771d85fd938a9a1965d4959b7bdf107f71ef
kly001 <karenly1961@gmail.com>
2020-10-08 15:24:51 -0400

    filterByWord working

--- index.js
+++ index.js
@@ -167,9 +167,16 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(arr){
+    let newArray = [];
+     for (let i=0; i<arr.length; i++){
+        if (arr[i]=== "Chocolate"){
+           newArray.push(arr[i])
+           return (newArray)
+        } 
+     } 
 }
+console.log(filterByWord(originalFlavors))
 
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
